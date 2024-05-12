9a365268e917f124adfa1ffe28a49734b5f767d0
mursidin14 <54929754+mursidin14@users.noreply.github.com>
2024-05-12 16:19:15 +0800

    cotegories migration

---
+++ database/migrations/2024_05_12_072205_create_categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->string('id', 100)->nullable(false)->primary();
+            $table->string('name', 100)->nullable(false);
+            $table->text('description')->nullable(true);
+            $table->timestamp('created_at')->nullable(false)->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
